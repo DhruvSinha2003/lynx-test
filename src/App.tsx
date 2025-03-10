e4bf44d2f3248f0d851cdb281a313bd60c1a5653
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-03-10 23:59:34 +0530

    vid not working

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,14 @@ export function App() {
       <image auto-size style="width:100px;" src={coffee} className="coffee" />
       <image auto-size style="width:100px;" src={lisa} className="lisa" />
       <image auto-size style="width:100px;" src={me} className="me" />
+      <video
+        auto-size
+        style={{ width: "100px" }}
+        src={wow}
+        className="wow"
+        controls
+        loop
+      />
     </view>
   );
 }
